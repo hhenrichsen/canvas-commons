a0e3aa0e924a0e3d77f6a594742319c7a3c7051c
Hunter Henrichsen <hunter.henrichsen@gmail.com>
2024-03-21 01:10:14 -0600

    :sparkles: Create DistortedCurve functional component

---
+++ src/components/DistortedCurve.tsx
@@ -0,0 +1,36 @@
+import {deref} from '@Util';
+import {Curve, Line, LineProps} from '@motion-canvas/2d';
+import {Reference, createComputed, range, useRandom} from '@motion-canvas/core';
+
+export function DistortedCurve(props: {
+  curve: Curve | Reference<Curve>;
+  displacement?: number;
+  count?: number;
+  samples?: number;
+  lineProps: LineProps;
+}) {
+  const c = deref(props.curve);
+  const points = createComputed(() =>
+    range((props.samples ?? 100) + 1).map(
+      i => c.getPointAtPercentage(i / props.samples).position,
+    ),
+  );
+  const dpl = props.displacement ?? 10;
+  const displacementMaps: [number, number][][] = range(props.count ?? 1).map(
+    () =>
+      range(1 + (props.samples ?? 100)).map(() => [
+        useRandom().nextFloat(-dpl, dpl),
+        useRandom().nextFloat(-dpl, dpl),
+      ]),
+  );
+  return (
+    <>
+      {range(props.count ?? 1).map(ci => {
+        const displaced = createComputed(() =>
+          points().map((p, pi) => p.add(displacementMaps[ci][pi])),
+        );
+        return <Line {...props.lineProps} points={displaced} closed></Line>;
+      })}
+    </>
+  );
+}
