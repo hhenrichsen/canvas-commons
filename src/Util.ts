37402bc4651a36602dc2697f780d88203f2994eb
Hunter Henrichsen <hunter.henrichsen@gmail.com>
2024-05-12 19:26:48 -0600

    :sparkles: Add drawIn utility

--- src/Util.ts
+++ src/Util.ts
@@ -1,10 +1,12 @@
-import {Layout, View2D} from '@motion-canvas/2d';
+import {Curve, Layout, PossibleCanvasStyle, View2D} from '@motion-canvas/2d';
 import {
   Reference,
   SignalValue,
   SimpleSignal,
   Vector2,
+  all,
   createSignal,
+  delay,
   unwrap,
 } from '@motion-canvas/core';
 
@@ -27,4 +29,33 @@ export function signalRef<T>(): {ref: Reference<T>; signal: SimpleSignal<T>} {
 
 export function signum(value: number): number {
   return value > 0 ? 1 : value < 0 ? -1 : 0;
+}
+
+export function* drawIn(
+  nodeOrRef: SignalValue<Curve>,
+  stroke: PossibleCanvasStyle,
+  fill: PossibleCanvasStyle,
+  duration: number,
+  restoreStroke: boolean = false,
+  defaultStrokeWidth: number = 4,
+) {
+  const node = unwrap(nodeOrRef);
+  const prevStroke = node.stroke();
+  const oldStrokeWidth = node.lineWidth();
+  const strokeWidth =
+    node.lineWidth() > 0 ? node.lineWidth() : defaultStrokeWidth;
+  node.end(0);
+  node.lineWidth(strokeWidth);
+  node.stroke(stroke);
+  yield* node.end(1, duration * 0.7);
+  yield* node.fill(fill, duration * 0.3);
+  if (restoreStroke) {
+    yield delay(
+      duration * 0.1,
+      all(
+        node.lineWidth(oldStrokeWidth, duration * 0.7),
+        node.stroke(prevStroke, duration * 0.7),
+      ),
+    );
+  }
 }
