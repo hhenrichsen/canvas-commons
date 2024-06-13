d16fbae48cd7a32fe86750d0b9fc159259cc5cc0
Hunter Henrichsen <hunter.henrichsen@gmail.com>
2024-06-13 10:50:05 -0600

    :memo: Update example scene

--- test/scenes/example.tsx
+++ test/scenes/example.tsx
@@ -1,6 +1,7 @@
 import {makeScene2D} from '@motion-canvas/2d/lib/scenes';
 import {waitFor, waitUntil} from '@motion-canvas/core/lib/flow';
 import {
+  Circle,
   Code,
   Gradient,
   Knot,
@@ -8,8 +9,9 @@ import {
   LezerHighlighter,
   Rect,
   Spline,
+  Txt,
 } from '@motion-canvas/2d';
-import {createRef} from '@motion-canvas/core';
+import {createRef, linear, range, useRandom} from '@motion-canvas/core';
 import {Scrollable} from '@components/Scrollable';
 import {WindowStyle, Window} from '@components/Window';
 import {Colors} from '@Colors';
@@ -19,6 +21,8 @@ import {parser as javascript} from '@lezer/javascript';
 import {CatppuccinMochaHighlightStyle} from '@highlightstyle/Catppuccin';
 import {CodeLineNumbers} from '@components/CodeLineNumbers';
 import {Terminal} from '@components/Terminal';
+import {Table, TableData, TableRow} from '@components/Table';
+import {Plot, LinePlot, ScatterPlot} from '@index';
 
 export default makeScene2D(function* (view) {
   const code = createRef<Code>();
@@ -308,4 +312,162 @@ class Cat {
 
   yield* waitFor(2);
   yield* terminalWindow().close(view, 1);
+
+  const table = createRef<Table>();
+  view.add(
+    <Table lineWidth={2} ref={table} opacity={0}>
+      <TableRow>
+        <TableData>
+          <Txt fill={'white'} width={'100%'} textAlign={'center'}>
+            1
+          </Txt>
+        </TableData>
+        <TableData>
+          <Txt fill={'white'} width={'100%'} textAlign={'center'}>
+            2
+          </Txt>
+        </TableData>
+        <TableData>
+          <Txt fill={'white'} width={'100%'} textAlign={'center'}>
+            3
+          </Txt>
+        </TableData>
+      </TableRow>
+      <TableRow>
+        <TableData>
+          <Txt fill={'white'} width={'100%'} textAlign={'center'}>
+            asdfghjkl;
+          </Txt>
+        </TableData>
+        <TableData>
+          <Txt fill={'white'} width={'100%'} textAlign={'center'}>
+            2
+          </Txt>
+        </TableData>
+        <TableData>
+          <Circle size={50} fill={Colors.Catppuccin.Mocha.Blue}></Circle>
+        </TableData>
+      </TableRow>
+    </Table>,
+  );
+
+  yield* table().opacity(1, 1);
+  yield* waitFor(1);
+  yield* table().opacity(0, 1);
+
+  const random = useRandom();
+
+  const plot = createRef<Plot>();
+  view.add(
+    <Plot
+      size={500}
+      ref={plot}
+      labelX="Time"
+      labelY="Beans"
+      labelSize={10}
+      opacity={0}
+    >
+      <LinePlot
+        lineWidth={4}
+        stroke={'red'}
+        data={range(0, 26).map(i => [i * 4, random.nextInt(0, 100)])}
+      />
+      ,
+    </Plot>,
+  );
+
+  yield* plot().opacity(1, 2);
+  yield* waitFor(2);
+
+  yield* plot().ticks(20, 3);
+  yield* plot().tickLabelSize(20, 2);
+  yield* plot().size(800, 2);
+  yield* plot().labelSize(30, 2);
+  yield* plot().min(-100, 2);
+  yield* plot().opacity(0, 2);
+  plot().remove();
+
+  const plot2 = createRef<Plot>();
+  const line2 = createRef<LinePlot>();
+  view.add(
+    <Plot
+      clip
+      size={500}
+      ref={plot2}
+      labelSize={0}
+      min={[-Math.PI * 2, -2]}
+      max={[Math.PI * 2, 2]}
+      labelFormatterX={x => `${Math.round(x / Math.PI)}π`}
+      ticks={[4, 4]}
+      opacity={0}
+    >
+      <LinePlot lineWidth={4} stroke={'red'} end={0} ref={line2} />
+    </Plot>,
+  );
+
+  line2().data(plot2().makeGraphData(0.1, x => Math.sin(x)));
+
+  yield* plot2().opacity(1, 2);
+  yield* waitFor(2);
+  yield* line2().end(1, 1);
+  yield* waitFor(3);
+
+  yield* plot2().opacity(0, 2);
+
+  const plot3 = createRef<Plot>();
+  const scatter3 = createRef<ScatterPlot>();
+  view.add(
+    <Plot
+      size={500}
+      ref={plot3}
+      labelX="Time"
+      labelY="Errors"
+      labelSize={10}
+      opacity={0}
+    >
+      <ScatterPlot
+        pointRadius={5}
+        pointColor={'red'}
+        ref={scatter3}
+        start={0.5}
+        end={0.5}
+        data={range(0, 26).map(i => [i * 4, random.nextInt(0, 100)])}
+      />
+    </Plot>,
+  );
+
+  yield* plot3().opacity(1, 2);
+  yield* waitFor(2);
+  yield scatter3().end(1, 3, linear);
+  yield* waitFor(0.1);
+  yield* scatter3().start(0, 3, linear);
+  yield* waitFor(2);
+  yield* plot3().opacity(0, 2);
+
+  const plot4 = createRef<Plot>();
+  const line4 = createRef<LinePlot>();
+  view.add(
+    <Plot
+      clip
+      size={500}
+      ref={plot4}
+      labelSize={0}
+      minX={-10}
+      maxX={10}
+      minY={-2}
+      maxY={50}
+      opacity={0}
+      ticks={[4, 4]}
+      offset={[-1, 0]}
+    >
+      <LinePlot lineWidth={4} stroke={'red'} ref={line4} />
+    </Plot>,
+  );
+
+  line4().data(plot4().makeGraphData(0.1, x => Math.pow(x, 2)));
+  yield* plot4().opacity(1, 2);
+  yield* waitFor(2);
+  yield* line4().end(1, 1);
+
+  yield* waitFor(5);
 });
